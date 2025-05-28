60cfbbeeb6ec47d4d5ab44f0ff53ed97dd80e69d
Falk Mielke <falk.mielke@inbo.be>
2025-05-28 11:35:49 +0200

    shiwasa: centralize library imports

--- R/import_aquatic_positioning.R
+++ R/import_aquatic_positioning.R
@@ -3,6 +3,30 @@
 # this script facilitates importing of the adjacent files
 # which are relevant for the aquatic observation well positioning
 
+
+# commonly used libraries (extensive list)
+conflictRules("n2khab", exclude = c("read_schemes", "read_scheme_types"))
+
+void <- suppressPackageStartupMessages
+library("stringr")     |> void() # string ragging
+library("dplyr")       |> void() # our favorite data wrangling toolbox
+library("tidyr")       |> void() # data preparation and rearrangement
+library("googledrive") |> void() # google drive data to/fro
+library("inbospatial") |> void() # convenience functions for wfs and other queries
+library("sf")          |> void() # spatial feature processing
+library("terra")       |> void() # spatial raster data
+library("mapview")     |> void() # show spatial features on a map
+library("n2khab")      |> void() # n2khab data and common functions
+library("n2khabmon")   |> void() # monitoring schemes for natura2000 habitats
+
+mapviewOptions(fgb = FALSE) # https://stackoverflow.com/a/65485896
+
+# print sample from sf objects
+kableprint <- function(df, show_rows = 5) {
+  knitr::kable(df[sample(seq_len(nrow(df)), show_rows), ])
+}
+
+
 # initialization
 source(here::here("..", "R", "confirm_n2khab_data_consistency.R"))
 source(here::here("..", "R", "google_drive_init.R"))
