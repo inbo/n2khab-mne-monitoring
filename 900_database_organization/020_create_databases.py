06c9bfe73b6094f8efaa0c3a68f0d27c408a7d3f
Falk Mielke <falk.mielke@inbo.be>
2025-06-06 11:48:03 +0200

    dbinit: inbound table "visits"

--- 900_database_organization/020_create_databases.py
+++ 900_database_organization/020_create_databases.py
@@ -5,7 +5,7 @@ import GenerateDatabase as GDB
 # https://docs.google.com/spreadsheets/d/12dWpyS2Wsjog3-z3q6-pUzlAnY4MuBbh6igDWH9bEZw/edit?usp=drive_link
 # database: loceval
 base_folder = GDB.PL.Path(".")
-GDB.ODStoCSVs(base_folder/"loceval_outbound.ods", base_folder/"db_structure")
+GDB.ODStoCSVs(base_folder/"loceval_dbstructure.ods", base_folder/"db_structure")
 
 db_connection = GDB.ConnectDatabase("inbopostgis_server.conf")
 db = GDB.Database( \
