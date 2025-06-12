796ad319ecc42fcb29b1a2b9dd0f5ab6610f0816
Falk Mielke <falk.mielke@inbo.be>
2025-06-12 16:40:08 +0200

    dbinit: production and dev databases

--- 900_database_organization/020_create_databases.py
+++ 900_database_organization/020_create_databases.py
@@ -2,17 +2,46 @@
 
 import GenerateDatabase as GDB
 
+
 # https://docs.google.com/spreadsheets/d/12dWpyS2Wsjog3-z3q6-pUzlAnY4MuBbh6igDWH9bEZw/edit?usp=drive_link
 # database: loceval
 base_folder = GDB.PL.Path(".")
-GDB.ODStoCSVs(base_folder/"loceval_dbstructure.ods", base_folder/"db_structure")
+GDB.ODStoCSVs(base_folder/"loceval_dev_dbstructure.ods", base_folder/"devdb_structure")
 
-db_connection = GDB.ConnectDatabase("inbopostgis_server.conf")
+db_connection = GDB.ConnectDatabase(
+    "inbopostgis_server.conf",
+    connection_config = "inbopostgis-dev",
+    database = "loceval_dev"
+)
 db = GDB.Database( \
-    base_folder = "./db_structure", \
+    base_folder = "./devdb_structure", \
     definition_csv = "TABLES.csv", \
     lazy_creation = False, \
     db_connection = db_connection \
 )
+
+# SET search_path TO public,"metadata","outbound","inbound";
+
+
+if False:
+    #### TODO prompt accidental overwrite
+
+    # https://docs.google.com/spreadsheets/d/12dWpyS2Wsjog3-z3q6-pUzlAnY4MuBbh6igDWH9bEZw/edit?usp=drive_link
+    # database: loceval
+    base_folder = GDB.PL.Path(".")
+    GDB.ODStoCSVs(base_folder/"loceval_dbstructure.ods", base_folder/"db_structure")
+
+    db_connection = GDB.ConnectDatabase(
+        "inbopostgis_server.conf",
+        connection_config = "inbopostgis",
+        database = "loceval_dev"
+    )
+    db = GDB.Database( \
+        database = "loceval", \
+        base_folder = "./db_structure", \
+        definition_csv = "TABLES.csv", \
+        lazy_creation = False, \
+        db_connection = db_connection \
+    )
 
 # SET search_path TO public,"metadata","outbound","inbound";
