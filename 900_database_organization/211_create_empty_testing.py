55b77fd36327a9ae76be6b07af7ce7950115a116
Falk Mielke <falk.mielke@inbo.be>
2025-06-27 14:26:00 +0200

    dbinit: (tangle file rename)

--- 900_database_organization/211_create_empty_testing.py
+++ 900_database_organization/211_create_empty_testing.py
@@ -2,15 +2,16 @@ import MNMDatabaseToolbox as DTB
 
 # database:
 base_folder = DTB.PL.Path(".")
-DTB.ODStoCSVs(base_folder/"loceval_dbstructure.ods", base_folder/"db_structure")
+structure_folder = base_folder/"devdb_structure"
+DTB.ODStoCSVs(base_folder/"loceval_dbstructure.ods", structure_folder)
 
 db_target = DTB.ConnectDatabase(
     "inbopostgis_server.conf",
-    connection_config = "inbopostgis-dev",
+    connection_config = "testing",
     database = "loceval_testing"
     )
 db = DTB.Database( \
-    structure_folder = "./devdb_structure", \
+    structure_folder = structure_folder, \
     definition_csv = "TABLES.csv", \
     lazy_creation = False, \
     db_connection = db_target, \
