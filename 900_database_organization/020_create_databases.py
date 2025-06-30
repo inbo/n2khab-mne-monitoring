1efd23ac65576bbb1a2d48e2c533bcbfe4cce7c8
Falk Mielke <falk.mielke@inbo.be>
2025-06-30 10:19:35 +0200

    dbinit: successful preservation of opho data

--- 900_database_organization/020_create_databases.py
+++ 900_database_organization/020_create_databases.py
@@ -2,10 +2,12 @@
 
 import MNMDatabaseToolbox as DTB
 
+# https://docs.google.com/spreadsheets/d/12dWpyS2Wsjog3-z3q6-pUzlAnY4MuBbh6igDWH9bEZw/edit?usp=drive_link
+# SET search_path TO public,"metadata","outbound","inbound";
 
-if True:
-    # https://docs.google.com/spreadsheets/d/12dWpyS2Wsjog3-z3q6-pUzlAnY4MuBbh6igDWH9bEZw/edit?usp=drive_link
-    # database: loceval
+
+if False:
+    # database: loceval_dev
     base_folder = DTB.PL.Path(".")
     structure_folder = base_folder/"devdb_structure"
     DTB.ODStoCSVs(base_folder/"loceval_dev_dbstructure.ods", structure_folder)
@@ -23,13 +25,32 @@ if True:
         tabula_rasa = False
         )
 
-    # SET search_path TO public,"metadata","outbound","inbound";
 
 
 if False:
+    # database: loceval_testing
+    base_folder = DTB.PL.Path(".")
+    structure_folder = base_folder/"devdb_structure"
+    DTB.ODStoCSVs(base_folder/"loceval_dev_dbstructure.ods", structure_folder)
+
+    db_connection = DTB.ConnectDatabase(
+        "inbopostgis_server.conf",
+        connection_config = "testing",
+        database = "loceval_testing"
+        )
+    db = DTB.Database( \
+        structure_folder = structure_folder, \
+        definition_csv = "TABLES.csv", \
+        lazy_creation = False, \
+        db_connection = db_connection, \
+        tabula_rasa = True
+        )
+
+
+
+if True:
     #### TODO prompt accidental overwrite
 
-    # https://docs.google.com/spreadsheets/d/12dWpyS2Wsjog3-z3q6-pUzlAnY4MuBbh6igDWH9bEZw/edit?usp=drive_link
     # database: loceval
     base_folder = DTB.PL.Path(".")
     structure_folder = base_folder/"db_structure"
@@ -45,6 +66,6 @@ if False:
         definition_csv = "TABLES.csv", \
         lazy_creation = False, \
         db_connection = db_connection \
+        , tabula_rasa = False
     )
 
-# SET search_path TO public,"metadata","outbound","inbound";
