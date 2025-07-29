2a2a0381944a44e2de76c2eda8ae332dff4c9f5a
Falk Mielke <falk.mielke@inbo.be>
2025-07-29 08:54:00 +0200

    dbinit: coordinate style to maps

---
+++ 900_database_organization/query_drafts/view_coordinates.sql
@@ -0,0 +1,23 @@
+
+DROP VIEW IF EXISTS  "outbound"."LocationCoords";
+CREATE VIEW "outbound"."LocationCoords" AS
+SELECT
+  LOC.*,
+  COORDS.wgs84_x,
+  COORDS.wgs84_y,
+  COORDS.lambert_x,
+  COORDS.lambert_y,
+  '<a href="https://www.google.com/maps/dir/?api=1&destination=' ||
+    CAST(COORDS.wgs84_y AS VARCHAR) ||
+    '%2C' ||
+    CAST(COORDS.wgs84_x AS VARCHAR) ||
+    '&travelmode=driving"> to google </a>'
+    AS google_link
+FROM "metadata"."Locations" AS LOC
+LEFT JOIN "metadata"."Coordinates" AS COORDS
+  ON COORDS.location_id = LOC.location_id
+WHERE LOC.location_id IS NOT NULL
+  AND COORDS.location_id IS NOT NULL
+;
+
+GRANT SELECT ON  "outbound"."LocationCoords"  TO  tom,yglinga,jens,lise,wouter,floris,karen,ward;
--- 900_database_organization/surgery/20250728_coordinates.sql
+++ 900_database_organization/surgery/20250728_coordinates.sql
@@ -86,6 +86,7 @@ SELECT * FROM "metadata"."Coordinates";
 
 
 
+
 DROP VIEW IF EXISTS  "outbound"."LocationCoords";
 CREATE VIEW "outbound"."LocationCoords" AS
 SELECT
@@ -93,7 +94,13 @@ SELECT
   COORDS.wgs84_x,
   COORDS.wgs84_y,
   COORDS.lambert_x,
-  COORDS.lambert_y
+  COORDS.lambert_y,
+  '<a href="https://www.google.com/maps/dir/?api=1&destination=' ||
+    CAST(COORDS.wgs84_y AS VARCHAR) ||
+    '%2C' ||
+    CAST(COORDS.wgs84_x AS VARCHAR) ||
+    '&travelmode=driving"> to google </a>'
+    AS google_link
 FROM "metadata"."Locations" AS LOC
 LEFT JOIN "metadata"."Coordinates" AS COORDS
   ON COORDS.location_id = LOC.location_id
