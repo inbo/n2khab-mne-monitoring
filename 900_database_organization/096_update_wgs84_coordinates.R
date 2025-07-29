466b5f4343792d82f702293275fc91b7c3bf073c
Falk Mielke <falk.mielke@inbo.be>
2025-07-29 08:54:00 +0200

    dbinit: coordinate style to maps

--- 900_database_organization/096_update_wgs84_coordinates.R
+++ 900_database_organization/096_update_wgs84_coordinates.R
@@ -5,39 +5,51 @@ source("MNMDatabaseToolbox.R")
 # keyring::key_set("DBPassword", "db_user_password") # <- for source database
 
 config_filepath <- file.path("./inbopostgis_server.conf")
-database_label <- "mnmgwdb"
-db_name <- glue::glue("{database_label}_testing")
-connection_profile <- glue::glue("{database_label}-testing")
-
-# database connection
-db_connection <- connect_database_configfile(
-  config_filepath = config_filepath,
-  profile = connection_profile
-)
 
 
-### load locations
-locations_sf <- sf::st_read(
-  db_connection,
-  DBI::Id("metadata", "Locations")
-  ) %>%
-  select(-ogc_fid) %>%
-  collect
+update_location_coordinates <- function(database_label, testing = TRUE) {
+  # db_name <- glue::glue("{database_label}_testing")
 
-locations_bd72 <- cbind(locations_sf, sf::st_coordinates(locations_sf)) %>%
-  rename(lambert_x = X, lambert_y = Y)
+  if (testing) {
+    connection_profile <- glue::glue("{database_label}-testing")
+  } else {
+    connection_profile <- glue::glue("{database_label}")
+  }
 
-locations_wgs84 <- sf::st_transform(locations_bd72, "EPSG:4326")
+  # database connection
+  db_connection <- connect_database_configfile(
+    config_filepath = config_filepath,
+    profile = connection_profile
+  )
 
-locations <- cbind(
-    sf::st_drop_geometry(locations_wgs84),
-    sf::st_coordinates(locations_wgs84)
-  ) %>%
-  rename(wgs84_x = X, wgs84_y = Y)
+  ### load locations
+  locations_sf <- sf::st_read(
+    db_connection,
+    DBI::Id("metadata", "Locations")
+    ) %>%
+    select(-ogc_fid) %>%
+    collect
 
-append_tabledata(
-  db_connection,
-  DBI::Id(schema = "metadata", table = "Coordinates"),
-  locations,
-  reference_columns = "location_id"
-)
+  locations_bd72 <- cbind(locations_sf, sf::st_coordinates(locations_sf)) %>%
+    rename(lambert_x = X, lambert_y = Y)
+
+  locations_wgs84 <- sf::st_transform(locations_bd72, "EPSG:4326")
+
+  locations <- cbind(
+      sf::st_drop_geometry(locations_wgs84),
+      sf::st_coordinates(locations_wgs84)
+    ) %>%
+    rename(wgs84_x = X, wgs84_y = Y)
+
+  append_tabledata(
+    db_connection,
+    DBI::Id(schema = "metadata", table = "Coordinates"),
+    locations,
+    reference_columns = "location_id"
+  )
+
+}
+
+
+update_location_coordinates("mnmgwdb", testing = FALSE)
+update_location_coordinates("loceval", testing = FALSE)
