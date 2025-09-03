11c4a87e385d4786d80c55a431b13c611af5dc0f
Falk Mielke <falk.mielke@inbo.be>
2025-09-03 12:20:07 +0200

    dbinit_pocupdate: identified 871030 LocationInfos

--- 900_database_organization/surgery/202509_poc_overhaul.sql
+++ 900_database_organization/surgery/202509_poc_overhaul.sql
@@ -4,9 +4,3 @@ ALTER TABLE "outbound"."FieldActivityCalendar" ADD COLUMN domain_part varchar;
 COMMENT ON COLUMN "outbound"."FieldActivityCalendar".domain_part IS E'domain partition';
 
 
--- there is a problem with grts_address = 871030
--- it disappeared, except from LocationInfos, Coordinates, and Locations
-SELECT * FROM "metadata"."Coordinates" WHERE grts_address = 871030;
-SELECT * FROM "outbound"."LocationInfos" WHERE grts_address = 871030;
-SELECT * FROM "metadata"."Locations" WHERE grts_address = 871030;
--- temporarily deleted on "staging"
