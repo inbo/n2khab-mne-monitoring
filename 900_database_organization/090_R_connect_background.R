0687a7a11758650b784461ee7f467e0b7d2ae8aa
Falk Mielke <falk.mielke@inbo.be>
2025-10-14 15:32:01 +0200

    dbinit: journal, YAD finetune and run

--- 900_database_organization/090_R_connect_background.R
+++ 900_database_organization/090_R_connect_background.R
@@ -11,9 +11,19 @@ config_filepath <- file.path("./inbopostgis_server.conf")
 
 ### connect to database
 ###
-init_keyring(keyring_label = "mnmdb_temp")
-unlock_keyring(keyring_label = "mnmdb_temp")
-get_mnm_password(
-  username = "falk",
-  keyring_label = "mnmdb_temp"
-)
+keyring_label <- 'mnmdb_temp'
+# note that you can create two keyrings of the same name! (shadowing)
+# avoid stacking keyrings with the same name
+if (keyring_label %in% keyring::keyring_list()$keyring) {
+  stop(glue::glue("Keyring Conflict: `{keyring_label}` already exists."))
+}
+
+# silent, single-prompt creation
+suppressWarnings(keyring::keyring_create(keyring_label, password = ""))
+
+# unlock it to schedule lock
+unlock_keyring(keyring_label = keyring_label)
+
+while (TRUE) {
+   Sys.sleep(5)
+}
