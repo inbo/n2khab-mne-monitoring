4643d39d011acdaa782d8b7ce1c837ad793e4563
Falk Mielke <falk.mielke@inbo.be>
2025-12-03 08:09:51 +0100

    dbinit: adjust reuse-fields

--- 900_database_organization/surgery/20251202_new_field_reuse.sql
+++ 900_database_organization/surgery/20251202_new_field_reuse.sql
@@ -1,32 +1,34 @@
 
+-- ALTER TABLE "inbound"."WellInstallationActivities" ADD COLUMN reused_well_reference varchar;
+-- COMMENT ON COLUMN "inbound"."WellInstallationActivities".reused_well_reference IS E'if an existing installation was reused or refreshed, this is its reference';
+
+-- ALTER TABLE "inbound"."WellInstallationActivities" DROP COLUMN IF EXISTS reused_well_reference;
+
+ALTER TABLE "inbound"."WellInstallationActivities" ADD COLUMN reused_existing_well boolean DEFAULT FALSE;
+COMMENT ON COLUMN "inbound"."WellInstallationActivities".reused_existing_well IS E'flag if an existing installation was reused';
+
+ALTER TABLE "inbound"."WellInstallationActivities" ADD COLUMN reused_with_replacement boolean DEFAULT FALSE;
+COMMENT ON COLUMN "inbound"."WellInstallationActivities".reused_with_replacement IS E'whether an existing installation was reused and refreshed';
 
 
+UPDATE "inbound"."WellInstallationActivities"
+SET reused_existing_well = TRUE
+WHERE random_point_number = 0
+;
 
+SELECT * FROM "inbound"."WellInstallationActivities"
+WHERE watina_code_used_2_piezometer = 'LIEP014F'
+;
 
+UPDATE "inbound"."WellInstallationActivities"
+SET reused_with_replacement = TRUE
+WHERE random_point_number = 0
+  AND watina_code_used_2_piezometer = 'LIEP014F'
+;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ALTER TABLE "inbound"."WellInstallationActivities" ADD COLUMN reused_well_reference varchar;
-COMMENT ON COLUMN "inbound"."WellInstallationActivities".reused_well_reference IS E'if an existing installation was reused or refreshed, this is its reference';
-
-
+SELECT * FROM "inbound"."WellInstallationActivities"
+WHERE random_point_number = 0
+;
 -- views adjusted:
 --   gw_FieldWork
 --   gw_myFieldWork
