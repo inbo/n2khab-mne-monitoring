9d27ebda7eab2cdb682c93c0650f0f1f0e7c280f
Falk Mielke <falk.mielke@inbo.be>
2025-12-09 09:23:43 +0100

    dbinit: start search for archived installations

---
+++ 900_database_organization/079_quickcheck_poc.R
@@ -0,0 +1,73 @@
+
+
+source("MNMLibraryCollection.R")
+load_poc_common_libraries()
+
+tic <- function(toc) round(Sys.time() - toc, 1)
+toc <- Sys.time()
+load_poc_rdata(reload = FALSE, to_env = parent.frame())
+message(glue::glue("Good morning!
+  Loading the POC data took {tic(toc)} seconds today."
+))
+
+
+snippets_path <- "/data/git/n2khab-mne-monitoring_support"
+
+toc <- Sys.time()
+load_poc_code_snippets(snippets_path)
+message(glue::glue(
+  "... loading/executing the code snippets took {tic(toc)}s."
+))
+
+verify_poc_objects()
+
+
+#_______________________________________________________________________________
+# the whole calendar -> fag_stratum_grts_calendar
+
+
+#_______________________________________________________________________________
+# prioritization and subset -> fieldwork_2025_prioritization_by_stratum
+
+fieldwork_2025_prioritization_by_stratum %>%
+  filter(
+    # field_activity_group == "GWINSTPIEZWELL",
+    # grts_address %in% c(49896893, 21323197)
+    grts_address %in% c(2137206)
+  ) %>%
+  select(
+    domain_part,
+    grts_address,
+    stratum,
+    date_start,
+    field_activity_group,
+    rank,
+    priority
+  ) %>%
+  t() %>% knitr::kable()
+
+
+'
+
+SELECT
+  FWCAL.grts_address,
+  FWCAL.stratum,
+  FWCAL.date_start,
+  ACT.activity_group,
+  FWCAL.activity_rank,
+  FWCAL.priority,
+  FWCAL.archive_version_id,
+  VIS.date_visit,
+  VIS.visit_done
+FROM "outbound"."FieldworkCalendar" FWCAL, "inbound"."Visits" VIS
+LEFT JOIN (
+  SELECT DISTINCT activity_group_id, activity_group
+  FROM "metadata"."GroupedActivities"
+  GROUP BY activity_group_id, activity_group
+  ) AS ACT ON ACT.activity_group_id = VIS.activity_group_id
+WHERE
+  FWCAL.fieldworkcalendar_id = VIS.fieldworkcalendar_id
+  AND FWCAL.grts_address IN (2137206)
+;
+
+'
