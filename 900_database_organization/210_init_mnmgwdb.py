35e64854db447ab6609876fd49524a473a748726
Falk Mielke <falk.mielke@inbo.be>
2026-01-06 10:13:46 +0100

    dbinit: fix sync_mod schema

--- 900_database_organization/210_init_mnmgwdb.py
+++ 900_database_organization/210_init_mnmgwdb.py
@@ -5,9 +5,9 @@ import MNMDatabaseToolbox as DTB
 # https://docs.google.com/spreadsheets/d/12dWpyS2Wsjog3-z3q6-pUzlAnY4MuBbh6igDWH9bEZw/edit?usp=drive_link
 # SET search_path TO public,"metadata","outbound","inbound","archive";
 
-restore_dev = False
+restore_dev = True
 restore_staging = False
-restore_testing = True # tabula rasa; note that it requires `dev` roles but works on `prod` structure
+restore_testing = False # tabula rasa; note that it requires `dev` roles but works on `prod` structure
 
 base_folder = DTB.PL.Path(".")
 DTB.ODStoCSVs(base_folder/"mnmgwdb_dev_structure.ods", base_folder/"mnmgwdb_dev_structure")
