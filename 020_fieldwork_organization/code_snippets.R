b5190b7aee01260dcda0c0d131d76e2758d61c4c
Falk Mielke <falk.mielke@inbo.be>
2026-03-18 13:41:52 +0100

    snippet_functionization: rename grts columns

--- 020_fieldwork_organization/code_snippets.R
+++ 020_fieldwork_organization/code_snippets.R
@@ -107,6 +107,31 @@ lm21_join <- \(...) lxxx_join(..., relationship = "many-to-one")
 lm2m_join <- \(...) lxxx_join(..., relationship = "many-to-many")
 
 
+#' rename the column `grts_address_final` to `grts_address`
+#' (not used in these snippets, but afterwards)
+#'
+#' @param .data input data frame
+#' @param keep_original toggle retention of the `grts_address` as `grts_address_original`
+#'
+rename_grts_address_final_to_grts_address <- function(.data, keep_original = FALSE) {
+
+  .data %>%
+    relocate(grts_address_final, .after = grts_address) %>%
+    {
+      if (keep_original) {
+        # optionally rename the original grts address
+        . %>% rename(grts_address_original = grts_address)
+      } else {
+        # (otherwise, drop original)
+        . %>% select(-grts_address)
+      }
+    } %>%
+    # simply rename
+    rename(grts_address = grts_address_final) %>%
+    return()
+}
+
+
 #' Nesting scheme, panelset, targetpanel; optional unique flattening
 #'
 #' merging scheme:module_combo_code:panel_set:targetpanel, still distinguishing
